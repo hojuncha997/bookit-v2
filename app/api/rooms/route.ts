1eb45f04e3a14f2a7972f2a4c8d92d8fe84186b1
hjcha <hjcha@markany.com>
2024-09-06 21:00:35 +0900

    feat: 백엔드 API 테스트 + next-connect 사용 테스트

---
+++ app/api/rooms/route.ts
@@ -0,0 +1,23 @@
+import { allRooms } from "@/backend/controllers/roomControllers";
+import { createEdgeRouter } from "next-connect";
+import { NextRequest } from "next/server";
+
+interface RequestContext {
+  params: {
+    id: string;
+  };
+}
+
+const router = createEdgeRouter<NextRequest, RequestContext>();
+
+router.get(allRooms);
+
+export async function GET(request: NextRequest, ctx: RequestContext) {
+  return router.run(request, ctx);
+}
+
+// export async function GET(request: NextRequest) {
+//   return NextResponse.json({
+//     data: "hello",
+//   });
+// }
