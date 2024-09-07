f8b2908dd17cbbc7b51c7092740f162ecdea1041
hjcha <hjcha@markany.com>
2024-09-07 23:43:15 +0900

    feat: Room 모델 스키마 및 인터페이스 정의

---
+++ backend/models/room.ts
@@ -0,0 +1,193 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+/*
+
+작성한 순서
+
+ 몽구스 임포트
+ const roomSchema: Schema = new Schema({
+을 통해 스키마 정의.
+
+IRoom 인터페이스를 만들어서 IRoom을 Document로 사용할 수 있도록 함.
+
+location은 ILocation 인터페이스로 정의.
+
+images는 IImage 인터페이스로 정의.
+
+reviews는 IReview 인터페이스로 정의.
+
+IRoom 인터페이스를 다시 수정(IImage, IReview, ILocation 사용).
+*/
+
+export interface IImage {
+  public_id: string;
+  url: string;
+}
+
+export interface IReview {
+  user: mongoose.Schema.Types.ObjectId;
+  name: string;
+  rating: number;
+  comment: string;
+}
+
+export interface ILocation {
+  type: string;
+  coordinates: number[];
+  formattedAddress: string;
+  city: string;
+  state: string;
+  zipcode: string;
+  country: string;
+}
+
+export interface IRoom extends Document {
+  name: string;
+  description: string;
+  pricePerNight: number;
+  address: string;
+  location: ILocation;
+  guestCapacity: number;
+  numOfBeds: number;
+  isInternet: boolean;
+  isBreakFast: boolean;
+  isAirConditioned: boolean;
+  isPetsAllowed: boolean;
+  isRoomCleaning: boolean;
+  rating: number;
+  numOfReviews: number;
+  images: IImage[];
+  category: string;
+  reviews: IReview[];
+  user: mongoose.Schema.Types.ObjectId;
+  createdAt: Date;
+}
+
+const roomSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter room name"],
+    MaxLength: [200, "Room name cannot exceed 200 characters"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter room name"],
+  },
+  pricePerNight: {
+    type: String,
+    required: [true, "Please enter room price per night"],
+    default: 0.0,
+  },
+  address: {
+    type: String,
+    required: [true, "Please enter room address"],
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+    },
+    coordinates: {
+      type: [Number],
+      index: "2dsphere",
+    },
+    formattedAddress: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  guestCapacity: {
+    type: Number,
+    required: [true, "Please enter room guest capacity"],
+  },
+  numOfBeds: {
+    type: Number,
+    required: [true, "Please enter number of beds in room"],
+  },
+  isInternet: {
+    type: Boolean,
+    default: false,
+  },
+  isBreakFast: {
+    type: Boolean,
+    default: false,
+  },
+  isAirConditioned: {
+    type: Boolean,
+    default: false,
+  },
+  isPetsAllowed: {
+    type: Boolean,
+    default: false,
+  },
+  isRoomCleaning: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+
+  category: {
+    type: String,
+    required: [true, "Please enter room category"],
+    enum: {
+      values: ["King", "Single", "Twins"],
+      message: "Please select correct category for room",
+    },
+  },
+  reviews: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.models.Room ||
+  mongoose.model<IRoom>("Room", roomSchema);
