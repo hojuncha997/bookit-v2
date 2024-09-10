7b1ffdadee5a3c1a165b12318874a287d31cbc97
hjcha <hjcha@markany.com>
2024-09-10 18:34:54 +0900

    fix: 스키마 에러 메시지 수정

--- backend/models/room.ts
+++ backend/models/room.ts
@@ -71,7 +71,7 @@ const roomSchema: Schema = new Schema({
   },
   description: {
     type: String,
-    required: [true, "Please enter room name"],
+    required: [true, "Please enter room description"],
   },
   pricePerNight: {
     type: String,
