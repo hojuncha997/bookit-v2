1c32cfc67e708d53d44479deeb08c08bacd150ca
hjcha <hjcha@markany.com>
2024-09-12 22:10:57 +0900

    feat: data fetching & caching

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
 import Home from "@/components/Home";
 
-export default function HomePage() {
+// const getRooms = async () => {
+//   const res = await fetch("http://localhost:3000/api/rooms"); // 데이터가 캐시됨
+//   const data = await res.json();
+//   return data;
+// };
+
+const getRooms = async () => {
+  const res = await fetch("http://localhost:3000/api/rooms", {
+    cache: "no-store", //또는 no-cache
+  }); // 데이터가 캐시되지 않음
+  const data = await res.json();
+  return data;
+};
+
+export default async function HomePage() {
+  const rooms = await getRooms();
+  console.log("resPerPage", rooms.resPerPage); // 5
+
   return <Home />;
 }
