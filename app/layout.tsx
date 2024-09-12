37e3065636412a603d715d8c88d90dc9f9d6b0f8
hjcha <hjcha@markany.com>
2024-09-12 23:32:18 +0900

    chore: 미사용 파일 삭제에 의한 코드 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,16 +9,16 @@ import { GlobalProvider } from "./GlobalProvider";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -33,7 +33,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head />
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      {/* <body className={`${geistSans.variable} ${geistMono.variable}`}> */}
+      <body>
         <GlobalProvider>
           <Header />
           {children}
