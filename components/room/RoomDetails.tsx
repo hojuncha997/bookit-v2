8a74f38b7f5217129f1877e321e04eb5006b3cf6
hjcha <hjcha@markany.com>
2024-09-13 21:22:56 +0900

    feat: 룸 상세페이지 렌더링

--- components/room/RoomDetails.tsx
+++ components/room/RoomDetails.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+"use client";
 
-const RoomDetails = ({ data }) => {
-  return <div>RoomDetails</div>;
+import { IRoom } from "@/backend/models/room";
+import React from "react";
+import StartRatings from "react-star-ratings";
+import RoomImageSlider from "./RoomImageSlider";
+import RoomFeatures from "./RoomFeatures";
+import BookingDatePicker from "./BookingDatePicker";
+import ListReviews from "../review/ListReviews";
+import NewReview from "../review/NewReview";
+
+interface Props {
+  data: {
+    room: IRoom;
+  };
+}
+
+const RoomDetails = ({ data }: Props) => {
+  const room = data.room;
+
+  return (
+    <div className="container container-fluid">
+      <h2 className="mt-5">{room.name}</h2>
+      <p>{room.address}</p>
+
+      <div className="ratings mt-auto mb-3">
+        <StartRatings
+          rating={room?.rating}
+          starRatedColor="orange"
+          numberOfStars={5}
+          name="rating"
+          starDimension="22px"
+          starSpacing="2px"
+        />
+        <span className="no-of-reviews">({room?.numOfReviews})</span>
+      </div>
+
+      <RoomImageSlider images={room?.images} />
+
+      <div className="row my-5">
+        <div className="col-12 col-md-6 col-lg-8">
+          <h3>Description</h3>
+          <p>{room?.description}</p>
+
+          <RoomFeatures room={room} />
+        </div>
+
+        <div className="col-12 col-md-6 col-lg-4">
+          <BookingDatePicker room={room} />
+          // Room Map - TODO
+        </div>
+      </div>
+
+      <NewReview />
+      <ListReviews />
+    </div>
+  );
 };
 
 export default RoomDetails;
