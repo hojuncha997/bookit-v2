19aaf5c5074be5bf83991f8fbac9fe1066e8b0c9
hjcha <hjcha@markany.com>
2024-09-15 22:28:19 +0900

    feat: 회원등록API 작성 및 테스트+ 비번 해싱

--- backend/models/users.ts
+++ backend/models/users.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 export interface IUser extends mongoose.Document {
   name: string;
   email: string;
@@ -44,6 +44,18 @@ const userSchema: mongoose.Schema = new mongoose.Schema({
   },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
+});
+
+//  Encrypting password before saving the user
+userSchema.pre("save", async function (next) {
+  // pre는 미들웨어로, save는 document를 저장하기 전에 실행됨
+
+  if (!this.isModified("password")) {
+    // isModified는 몽구스 메소드로, 입력한 필드가 수정되었는지 확인. 회원가입의 경우는 모든 것이 처음 입력되기 때문에 true
+    next(); // 미들웨어 체인의 다음 함수로 넘기는 함수. 만약 password가 수정되지 않았다면 다음 함수로 넘어감(암호화 과정 생략)
+  }
+  // npm i bcryptjs --save로 우선 설치 필요
+  this.password = await bcrypt.hash(this.password, 10); // 비밀번호를 암호화.
 });
 
 export default mongoose.models.User ||
